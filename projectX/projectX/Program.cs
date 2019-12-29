65ddbd25474a702e900dd5231b0826caad5487b5
Artem Shnip <tjoma27@gmail.com>
2019-12-29 21:43:57 +0300

    add calculator class

--- projectX/projectX/Program.cs
+++ projectX/projectX/Program.cs
@@ -9,16 +9,14 @@ namespace ProjectX
         static void Main(string[] args)
         {
             ManagementEventWatcher startProgramm = new ManagementEventWatcher(
-                new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace WHERE ProcessName = \"calculator.exe\""));            
+                new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));            
             startProgramm.EventArrived += startWatch_class;
-            startProgramm.EventArrived += time_class;
             startProgramm.Start();
 
 
             ManagementEventWatcher stopProgramm = new ManagementEventWatcher(
-                new WqlEventQuery("SELECT * FROM Win32_ProcessStopTrace WHERE ProcessName = \"calculator.exe\""));
+                new WqlEventQuery("SELECT * FROM Win32_ProcessStopTrace"));
             stopProgramm.EventArrived += stopWatch_class;
-            stopProgramm.EventArrived += time_class;
             stopProgramm.Start();
             Console.WriteLine("Press ENTER to exit");
             Console.ReadLine();
@@ -27,22 +25,41 @@ namespace ProjectX
         }
         static void startWatch_class(object programm, EventArrivedEventArgs e)
         {
-            Console.WriteLine("Process started: {0}", e.NewEvent.Properties["ProcessName"].Value);
+            string name = e.NewEvent.Properties["ProcessName"].Value.ToString();
+            if (name == "Calculator.exe")
+            {
+                bool start = true;
+                Calculator_Class(start);
+            }
         }
         static void stopWatch_class(object programm, EventArrivedEventArgs e)
         {
-            Console.WriteLine("Process stopped: {0}", e.NewEvent.Properties["ProcessName"].Value);            
+            string name = e.NewEvent.Properties["ProcessName"].Value.ToString();
+            if (name == "Calculator.exe")
+            {
+                bool start = false;
+                Calculator_Class(start);
+            }
         }
-        static void time_class(object programm, EventArrivedEventArgs e)
+        static void Calculator_Class(bool start)
         {
-            DateTime time = DateTime.Now;
-            Console.WriteLine(time);
+            Stopwatch sekundomer = new Stopwatch();     //??????????????????????????
+            if (start == true)
+            {                
+                Console.Write("START  Calculator  ");
+                DateTime time = DateTime.Now;
+                Console.WriteLine(time);
+                sekundomer.Start();
+            }
+            else
+            {
+                Console.Write("STOP  Calculator  ");
+                DateTime time = DateTime.Now;
+                Console.WriteLine(time);
+                sekundomer.Stop();
+                TimeSpan ts = sekundomer.Elapsed;                
+                Console.WriteLine("\nсекундомер -- " + ts);
+            }
         }
-
-
-        //Object.MemberwiseClone создать объект,имя,время старта,время финиша
-        //если время старта не нал вкл секундомер,если время финиш не нал выкл??????????
-
-
     }
 }
