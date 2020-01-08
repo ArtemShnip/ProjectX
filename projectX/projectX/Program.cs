c7627e6b284d51e3fb83ed06cb47c9645a7ce729
ArtemShnip <47022618+ArtemShnip@users.noreply.github.com>
2020-01-08 19:36:55 +0300

    added name programs

--- projectX/projectX/Program.cs
+++ projectX/projectX/Program.cs
@@ -6,7 +6,9 @@ namespace ProjectX
 {
     class Program
     {
-        public static  Stopwatch sekundomer = new Stopwatch();
+        public static  Stopwatch sekundomerCal = new Stopwatch();
+        public static Stopwatch sekundomerAi = new Stopwatch();
+        public static Stopwatch sekundomerPS = new Stopwatch();
         public static string ID, nameStart, nameStop;
         public static string timeStart, timeStop;
        public static void Main(string[] args)
@@ -26,18 +28,43 @@ namespace ProjectX
         }
         static void startWatch_class(object programm, EventArrivedEventArgs e)
         {
+            //string name123 = e.NewEvent.Properties["ProcessName"].Value.ToString();
+            //string id123 = e.NewEvent.Properties["ProcessId"].Value.ToString();
+            //Console.WriteLine(" start  " + name123 + "------- " + id123);
+
             string cal = e.NewEvent.Properties["ProcessName"].Value.ToString();
             if (cal == "Calculator.exe")
             {
-                ID = e.NewEvent.Properties["ProcessId"].ToString();  //????????????????????????????????    HANDLE     ???????????????
+                ID = e.NewEvent.Properties["ProcessId"].Value.ToString();             //????????????????????????????????    HANDLE     ???????????????  Photoshop.exe
                 nameStart = e.NewEvent.Properties["ProcessName"].Value.ToString();
                 DateTime time = DateTime.Now;
                 timeStart = time.ToLongTimeString();
-                sekundomer.Start();
+                sekundomerCal.Start();
+            }
+            if (cal == "Illustrator.exe")
+            {
+                ID = e.NewEvent.Properties["ProcessId"].Value.ToString();
+                nameStart = e.NewEvent.Properties["ProcessName"].Value.ToString();
+                DateTime time = DateTime.Now;
+                timeStart = time.ToLongTimeString();
+                sekundomerAi.Start();
+            }
+            if (cal == "Photoshop.exe")
+            {
+                ID = e.NewEvent.Properties["ProcessId"].Value.ToString();
+                nameStart = e.NewEvent.Properties["ProcessName"].Value.ToString();
+                DateTime time = DateTime.Now;
+                timeStart = time.ToLongTimeString();
+                sekundomerPS.Start();
             }
         }
         static void stopWatch_class(object programm, EventArrivedEventArgs e)
         {
+
+            //string name123 = e.NewEvent.Properties["ProcessName"].Value.ToString();
+            //string id123 = e.NewEvent.Properties["ProcessId"].Value.ToString();
+            //Console.WriteLine(" stop  " + name123 + "------- " + id123);
+
             DateTime time = DateTime.Now;
             nameStop = e.NewEvent.Properties["ProcessName"].Value.ToString();
             timeStop = time.ToLongTimeString();
@@ -47,9 +74,21 @@ namespace ProjectX
         {
             if (nameStop == "Calculator.exe")
             {
-                sekundomer.Stop();
-                TimeSpan ts = sekundomer.Elapsed;
-                Console.WriteLine($@" {nameStart} started at {timeStart} closed at {timeStop} worked {ts}");
+                sekundomerCal.Stop();
+                TimeSpan ts = sekundomerCal.Elapsed;
+                Console.WriteLine($@" {nameStart} started at {timeStart} closed at {timeStop} worked {ts} --------------- {ID}");
+            }
+            if (nameStop == "Illustrator.ex")
+            {
+                sekundomerAi.Stop();
+                TimeSpan ts = sekundomerAi.Elapsed;
+                Console.WriteLine($@" {nameStart} started at {timeStart} closed at {timeStop} worked {ts} --------------- {ID}");
+            }
+            if (nameStop == "Photoshop.exe")
+            {
+                sekundomerPS.Stop();
+                TimeSpan ts = sekundomerPS.Elapsed;
+                Console.WriteLine($@" {nameStart} started at {timeStart} closed at {timeStop} worked {ts} --------------- {ID}");
             }
         }
     }
